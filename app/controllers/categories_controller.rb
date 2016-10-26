dc405d69e5a587adb6ce41f179c2f78ef4764ea6
mardiniii <sezama03@hotmail.com>
2016-10-25 19:38:08 -0500

    Add category entire CRUD

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,52 @@
 class CategoriesController < ApplicationController
+  before_action :find_category, only: [ :edit, :update, :show, :destroy ]
+
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = 'The category have been saved'
+      redirect_to category_path(id: @category.id, it_was: 'created')
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = 'Category was successfully updated.'
+      redirect_to category_path(id: @category.id, it_was: 'updated')
+    else
+      render :edit
+    end
   end
 
   def show
   end
 
   def destroy
+    @category.destroy
+    flash[:notice] = 'Category was successfully destroyed.'
+    redirect_to categories_path
   end
 
   def index
+    @categories = Category.all
   end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def category_params
+      params.require(:category).permit(:name, :description)
+    end
+
+    def find_category
+      @category = Category.find(params[:id])
+    end
 end
