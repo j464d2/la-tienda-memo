fe890f05b9b398afa4195097525fcdbfeb174bf9
mardiniii <sezama03@hotmail.com>
2016-10-28 20:39:02 -0500

    Add image to product model with paperclip

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,7 +46,7 @@ class ProductsController < ApplicationController
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :reference, :price, :quantity, :brand, :description, :category_id)
+      params.require(:product).permit(:name, :reference, :price, :quantity, :brand, :description, :category_id, :image)
     end
 
     def find_product
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,24 +2,32 @@
 #
 # Table name: products
 #
-#  id          :integer          not null, primary key
-#  name        :string
-#  reference   :string
-#  price       :decimal(, )
-#  quantity    :integer
-#  brand       :string
-#  description :text
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  category_id :integer
+#  id                 :integer          not null, primary key
+#  name               :string
+#  reference          :string
+#  price              :decimal(, )
+#  quantity           :integer
+#  brand              :string
+#  description        :text
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  category_id        :integer
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
 #
 
 class Product < ApplicationRecord
   # Associations
   belongs_to :category
 
+  # Paperclip
+  has_attached_file :image, styles: { thumb: "242x200#", micro_thumb: "50x50#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
   # Validations
-  validates :name, :reference, :price, :quantity, :brand, :category, presence: true
+  validates :name, :reference, :price, :quantity, :brand, :category, :image, presence: true
   validates :price, numericality: true
   validates :quantity, numericality: { only_integer: true }
   validates :price, :quantity, numericality: { greater_or_equal_to: 0 }
