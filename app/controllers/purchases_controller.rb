9e9554fd1916a7266e89d8331e678e22562b19e2
mardiniii <sezama03@hotmail.com>
2016-10-29 11:54:54 -0500

    Add create action for purchase model

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,23 @@
+class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: :create
+
+  def create
+    product = Product.find(params[:product_id])
+
+    purchase = Purchase.new
+    purchase.product = product
+    purchase.user = current_user
+    purchase.status = :pending
+    purchase.price = product.price
+
+    if product.available? && purchase.save
+      product.update_stock
+      flash[:notice] = 'Tu compra ha sido realizada con éxito'
+    else
+      flash[:alert] = 'No ha sido posible agregar este producto a tus compras, intenta de nuevo'
+    end
+
+    redirect_to root_path(category_id: product.category_id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :categories
   resources :products
+  resources :purchases
 
   root 'pages#home'
 end
